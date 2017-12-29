152cd47331eb3ff2ed0a62914ab66ba39020056b
denizdmu <32480513+denizdmu@users.noreply.github.com>
2017-12-29 20:30:29 +0000

    testin the site

--- javaScprit/script.js
+++
@@ -1,9 +0,0 @@
-
-            function initMap(){
-                var location = {lat: -25.363,lng: 131.044};
-                var map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 4,
-              centre: location
-           });      
-            
-          async defer src="htts://maps.googleapis.com/maps/api/js?key=AIzaSyAykWE7oKs7XDZfx7_Rx7dSmhBh45ejYEg&callback=initMap"
