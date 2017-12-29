518d04e50c67ec18d3edae070fce213461e9422c
denizdmu <32480513+denizdmu@users.noreply.github.com>
2017-12-29 23:11:21 +0000

    Adding a diffrent JavaScript file

---
+++ javaScprit/main.js
@@ -0,0 +1,28 @@
+//FUCTION FOR GOOGLE API
+
+function initMap() {
+        var uluru = {lat: 52.6369, lng: 1.1398};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+
+//FUCTION FOR SCOLLING UP BUTTON 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
